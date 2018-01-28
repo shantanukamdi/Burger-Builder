66762fbf8227d5f3292ce00e5ea84eda0c27baa7
Shantanu Kamdi <kamdishantanu@gmail.com>
2018-01-28 20:24:21 +0530

    Added more functionality

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     console.log('Object.keys(props.ingredients) '+Object.keys(props.ingredients));
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
                                         .map(igKey => {
                                             console.log('igKey'+igKey);
                                             return [...Array(props.ingredients[igKey])]
                                                     .map((_, i) => {
                                                        return <BurgerIngredient key={igKey + i} type={igKey} />
                                                     });
-                                        });
+                                        })
+                                        .reduce((arr, el) => {
+                                            return arr.concat(el);
+                                        }, []);
+    console.log('transformedIngredients',transformedIngredients);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = 'Please start adding ingredients';
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
